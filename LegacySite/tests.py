4a343bb7e208f2d4b6da9ed30a981ef093b961fb
ItzRobD <itzrobd@gmail.com>
2022-11-12 17:30:39 -0500

    part_2_complete
    committing part 2

--- LegacySite/tests.py
+++ LegacySite/tests.py
@@ -1,32 +1,3 @@
-from django.core.cache import cache
-from django.test import TestCase, Client
-import unittest
-from django.http import HttpResponse
-from django.test.utils import setup_test_environment, teardown_test_environment
-from django.urls import reverse
-
-teardown_test_environment()
-setup_test_environment()
+from django.test import TestCase
 
 # Create your tests here.
-class AppSecTests(TestCase):
-    def setup(self):
-        self.client = Client()
-
-    def test_xss(self):
-        cache.clear()
-        payload = "<script>alert('This is an XSS vulnerability')</script>"
-        #payload = 'this is a test'
-        params = {'director': payload}
-        response = self.client.get('/gift.html', params)
-
-        print(f"Response: {response}")
-        print(f'Response Code: {response.status_code}')
-        #print(f"Response content params:{response.content_params}")
-        #print(f"Response content type:{response.content_type}")
-        print(f"Response content: {response.content}")
-
-
-        #self.assertEqual(response.status_code, 200)
-
-        #self.assertEqual(response.context['director'], payload)
