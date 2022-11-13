04ec647421bc14b73b8a37eace2ebd6ecc52fce1
Rob Durst <itzrobd@gmail.com>
2022-11-13 17:16:16 -0500

    travis build test

--- LegacySite/tests.py
+++ LegacySite/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class AppSec2Tests(TestCase):
+	fixtures = ['fixture.json']
+
+	def setUp(self):
+		self.client = Client()
+		self.csrf_client = Client(enforce_csrf_checks=True)
+
+	def test_xss(self):
+		payload = "<script>alert('This is an XSS vulnerability')</script>"
+		params = {'director': payload}
+		response = self.client.get('/gift.html', params)
+
