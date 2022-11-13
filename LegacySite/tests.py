7a86d6c0269269d2e58119acc8619d48ce3cc626
Rob Durst <itzrobd@gmail.com>
2022-11-13 18:02:22 -0500

    Switched to using travis

--- LegacySite/tests.py
+++ LegacySite/tests.py
@@ -12,4 +12,16 @@ class AppSec2Tests(TestCase):
 		payload = "<script>alert('This is an XSS vulnerability')</script>"
 		params = {'director': payload}
 		response = self.client.get('/gift.html', params)
+        self.assertEqual(response.context.get('director', None), payload)
 
+	def test_csrf(self):
+		params = {'username': 'zerocool', 'amount' : '100'}
+		response = self.csrf_client.post('/gift/2', params)
+		self.assertEqual(response.status_code, 403)
+
+	def test_sql(self):
+		self.client.login(username='admin', password='adminpassword')
+		with open('part 1/attack.gftcrd') as f:
+			params = {'card_supplied': 'True', 'card_data': f}
+			response = self.client.post('/use.html', params)
+		self.assertEqual(response.context.get('card_found', None), None)
